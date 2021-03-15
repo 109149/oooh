f3ab960b842786d308adcd167930af801337d8ad
109149 <109149qwe@gmail.com>
2021-03-15 22:06:12 +0400

    test(app-test.js): mock fetch function

--- src/components/app/__tests__/App-test.js
+++ src/components/app/__tests__/App-test.js
@@ -2,21 +2,41 @@ import App from "../App";
 import { render, act, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-describe("<App>", function () {
-  let initialData = {
+describe("<App />", function () {
+  let triviaInitialData = {
     trivia: ["trivia 1", "trivia 2", "trivia 3", "trivia 4"],
     spoilers: ["spoiler 1", "spoiler 2", "spoiler 3", "spoiler 4"],
   };
-  beforeEach(function () {
-    global["fetch"] = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve(initialData),
-      })
-    );
-  });
 
+  let imageUrlsInitialData = {
+    imageUrls: ["img url 1", "img url 2", "img url 3", "img url 4"],
+  };
+
+  const mockFetch = (triviaData, imageUrlsData, id) => async (url) => {
+    // console.log("mockFetch", url);
+    switch (url) {
+      case `/trivia/${id || "ID1"}`: {
+        return { json: () => Promise.resolve(triviaData || triviaInitialData) };
+      }
+      case `/imageUrls/${id || "ID1"}`: {
+        return {
+          json: () => Promise.resolve(imageUrlsData || imageUrlsInitialData),
+        };
+      }
+      default: {
+        throw new Error(`Unhandled request: ${url}`);
+      }
+    }
+  };
+
+  beforeAll(() => {
+    jest.spyOn(window, "fetch");
+  });
+  beforeEach(function () {
+    window.fetch.mockImplementation(mockFetch());
+  });
   afterEach(function () {
-    global.fetch.mockRestore();
+    window.fetch.mockRestore();
   });
 
   it("should render normally", async function () {
@@ -30,8 +50,8 @@ describe("<App>", function () {
     });
 
     // assert initial DOM
-    expect(screen.getByRole(/textbox/)).toHaveAttribute("value");
-    expect(screen.getByRole(/button/)).toHaveTextContent("Search");
+    expect(screen.getByRole(/textbox/)).toHaveValue("");
+    expect(screen.getAllByRole(/button/)).toHaveLength(5);
 
     // assert initial state
     expect(screen.getAllByText(/trivia/)).toHaveLength(4);
@@ -40,23 +60,69 @@ describe("<App>", function () {
     userEvent.type(screen.getByRole(/textbox/), "ID2");
     expect(screen.getByRole(/textbox/)).toHaveValue("ID2");
 
-    // new mock data
-    const newData = {
+    // mock new data
+    let newTriviaData = {
       trivia: [],
       spoilers: [],
     };
+    let newImageUrlData = {
+      imageUrls: [],
+    };
 
     // mock fetch with new data
-    global["fetch"] = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve(newData),
-      })
+    window.fetch.mockImplementation(
+      mockFetch(newTriviaData, newImageUrlData, "ID2")
     );
 
     // assert new state
     await act(async function () {
-      userEvent.click(screen.getByRole(/button/));
+      userEvent.click(screen.getAllByRole(/button/)[0]); // get search button, which is first on the list
     });
+    expect(screen.getByRole(/textbox/)).toHaveValue("");
     expect(screen.queryAllByText(/trivia/)).toHaveLength(0);
+
+    // assert change of new id
+    userEvent.type(screen.getByRole(/textbox/), "ID3");
+    expect(screen.getByRole(/textbox/)).toHaveValue("ID3");
+
+    // mock new data
+    newTriviaData = {
+      trivia: ["new trivia"],
+      spoilers: ["new spoiler"],
+    };
+    newImageUrlData = {
+      imageUrls: ["new image url"],
+    };
+
+    // mock fetch with new data
+    window.fetch.mockImplementation(
+      mockFetch(newTriviaData, newImageUrlData, "ID3")
+    );
+
+    // assert new state
+    await act(async function () {
+      userEvent.click(screen.getAllByRole(/button/)[0]); // get search button, which is first on the list
+    });
+    expect(screen.getByRole(/textbox/)).toHaveValue("");
+    expect(screen.queryAllByText("new trivia")).toHaveLength(1);
+    expect(screen.queryAllByText("new spoiler")).toHaveLength(0);
+    expect(screen.getByRole("img").src).toMatch(
+      encodeURIComponent(newImageUrlData.imageUrls[0])
+    );
+
+    // assert change to initial id
+    userEvent.type(screen.getByRole(/textbox/), "ID1");
+    expect(screen.getByRole(/textbox/)).toHaveValue("ID1");
+
+    // mock fetch with initial data
+    window.fetch.mockImplementation(mockFetch(undefined, undefined, "ID3"));
+
+    // assert initial data
+    await act(async function () {
+      userEvent.click(screen.getAllByRole(/button/)[0]);
+    });
+    expect(screen.getByRole(/textbox/)).toHaveValue("");
+    expect(screen.queryAllByText(/trivia/)).toHaveLength(4);
+    expect(screen.getAllByRole("img")).toHaveLength(4);
   });
 });
