c87f1383feff31f5986ccc9f792fbba6df425395
109149 <109149qwe@gmail.com>
2021-03-16 22:28:57 +0400

    refactor: re-export component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import App from "./app/App";
 import Cards from "./cards/Cards";
 import Card from "./cards/Card";
+import Loading from "./loading/Loading";
 
-export { App, Cards, Card };
+export { App, Cards, Card, Loading };
