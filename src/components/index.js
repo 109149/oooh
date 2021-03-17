3e3fede5f14b860031d54f667458849b0f8f1b10
109149 <109149qwe@gmail.com>
2021-03-18 00:01:43 +0400

    refactor: re-export components

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import App from "./app/App";
 import Cards from "./cards/Cards";
 import Card from "./cards/Card";
 import Loading from "./loading/Loading";
+import ThemeToggler from "./theme/ThemeToggler";
 
-export { App, Cards, Card, Loading };
+export { App, Cards, Card, Loading, ThemeToggler };
