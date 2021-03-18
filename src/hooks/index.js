547a5fdb23db06b090f5183cb36485497ad8f4a4
109149 <109149qwe@gmail.com>
2021-03-18 10:08:39 +0400

    chore: allows you to access all hooks from one place

---
+++ src/hooks/index.js
@@ -0,0 +1,17 @@
+import { useImageUrls, useTrivia, useToggle } from "../components/app/hooks";
+import {
+  useDarkMode,
+  useLocalStorage,
+  usePrefersDarkMode,
+  useEventListener,
+} from "../components/theme/hooks";
+
+export {
+  useImageUrls,
+  useTrivia,
+  useToggle,
+  useDarkMode,
+  useLocalStorage,
+  usePrefersDarkMode,
+  useEventListener,
+};
